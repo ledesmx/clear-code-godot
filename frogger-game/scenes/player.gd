766ae998dd41d9d6ed641ca6eb73b914febcf19a
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-17 11:44:49 -0600

    add collisions to the player and trees

--- frogger-game/scenes/player.gd
+++ frogger-game/scenes/player.gd
@@ -1,10 +1,13 @@
-extends Node2D
+extends CharacterBody2D
 
 var direction := Vector2.ZERO
-var speed := 2
+var speed := 100
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed
+	velocity = direction * speed
+	move_and_slide()
 	if Input.is_action_just_pressed("print_something"):
-		print("Something")
+		speed = 160
+	if Input.is_action_just_released("print_something"):
+		speed = 100
