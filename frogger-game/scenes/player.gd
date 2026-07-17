025a2992c9273aa976730d512b1caf7ee7f9515e
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-17 10:52:22 -0600

    complete exercise: print somthing with input

--- frogger-game/scenes/player.gd
+++ frogger-game/scenes/player.gd
@@ -3,6 +3,8 @@ extends Node2D
 var direction := Vector2.ZERO
 var speed := 10
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	position += direction * speed
+	if Input.is_action_just_pressed("print_something"):
+		print("Something")
