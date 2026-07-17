8ad3af4d7ea0e6dd99e802a74fc4a77f48e57fce
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-16 20:12:53 -0600

    complete exercise to add a scene and scale it

--- frogger-game/scenes/player.gd
+++ frogger-game/scenes/player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var direction = Vector2(1, 0)
+var direction := Vector2.ZERO
+var speed := 10
 
 func _physics_process(delta: float) -> void:
-	position += direction * 10
+	position += direction * speed
