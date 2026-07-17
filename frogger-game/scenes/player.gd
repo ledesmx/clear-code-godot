4be6ec88b9383062ab5d44e0b209557a3459a307
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-17 10:34:33 -0600

    add movement with key inputs

--- frogger-game/scenes/player.gd
+++ frogger-game/scenes/player.gd
@@ -4,4 +4,5 @@ var direction := Vector2.ZERO
 var speed := 10
 
 func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
 	position += direction * speed
