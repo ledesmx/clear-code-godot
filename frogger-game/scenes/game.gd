c084c5f6f950f084675fd1dec851ede44607fa1d
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-20 21:53:11 -0600

    set layers for collisions

--- frogger-game/scenes/game.gd
+++ frogger-game/scenes/game.gd
@@ -3,12 +3,18 @@ extends Node2D
 var green_car_scene: PackedScene = preload("res://green_car.tscn")
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("hola")
-
-
 func _on_car_timer_timeout() -> void:
 	var car = green_car_scene.instantiate() as Area2D
 	var start_markers = $CarStartPositions.get_children().pick_random() as Marker2D
 	car.position = start_markers.position
 	$Objects.add_child(car)
+	
+	car.connect("body_entered", go_to_title)
+
+func go_to_title(body):
+	print(body)
+	print("Game over. Go to title")
+
+
+func _on_finish_area_body_entered(body: Node2D) -> void:
+	print("Winner")
