1586cc5f5646f5bb66813fa0eeadffce561dd532
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-24 10:11:49 -0600

    change mechanic for running

--- frogger-game/scenes/player.gd
+++ frogger-game/scenes/player.gd
@@ -4,16 +4,18 @@ var direction := Vector2.ZERO
 const WALK_SPEED := 30
 const RUN_SPEED := 60
 var speed := WALK_SPEED
+var fatigue := false
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	animate()
 	move_and_slide()
-	if Input.is_action_just_pressed("run"):
+	if Input.is_action_just_pressed("run") and not fatigue:
 		speed = RUN_SPEED
-	if Input.is_action_just_released("run"):
-		speed = WALK_SPEED
+		print("run")
+		$RunTimer.start()
+
 
 func animate():
 	if direction:
@@ -23,3 +25,15 @@ func animate():
 			$AnimatedSprite2D.animation = "down" if direction.y > 0 else "up"
 	else:
 		$AnimatedSprite2D.frame = 0
+
+
+func _on_run_timer_timeout() -> void:
+	speed = WALK_SPEED
+	fatigue = true
+	print(fatigue)
+	$RunWaitTimer.start()
+
+
+func _on_run_wait_timer_timeout() -> void:
+	fatigue = false
+	print(fatigue)
