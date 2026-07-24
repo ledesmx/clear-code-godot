dbd518db9ead96b36469835de2126af7664be158
ledesmx <alberto.ledesma.dev@gmail.com>
2026-07-23 19:50:21 -0600

    change speed of player

--- frogger-game/scenes/player.gd
+++ frogger-game/scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var direction := Vector2.ZERO
-var speed := 100
+var speed := 30
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -9,9 +9,9 @@ func _physics_process(_delta: float) -> void:
 	animate()
 	move_and_slide()
 	if Input.is_action_just_pressed("print_something"):
-		speed = 160
+		speed *= 2
 	if Input.is_action_just_released("print_something"):
-		speed = 100
+		speed /= 2
 
 func animate():
 	if direction:
