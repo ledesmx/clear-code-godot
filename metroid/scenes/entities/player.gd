5eecfdf64152b5eb80894e466e51968bac5c3011
ledesmx <alberto.ledesma.dev@gmail.com>
2026-08-05 20:31:24 -0600

    export gravity and jump strength

--- metroid/scenes/entities/player.gd
+++ metroid/scenes/entities/player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
 var speed := 100
+@export var gravity = 9.8
+@export var jump_strength = 500
 
 func _physics_process(delta: float) -> void:
 	var direction_x := Input.get_axis("left", "right")
 	velocity.x = direction_x * speed
 	
 	if Input.is_action_just_pressed("jump"):
-		velocity.y -= 500
+		velocity.y -= jump_strength
 	
 	# gravity
-	velocity.y += 9.8
+	velocity.y += gravity
 	move_and_slide()
 	
