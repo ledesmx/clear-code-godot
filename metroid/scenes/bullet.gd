818a0e6c6128bcc64a479f1cf1e85e8742c05bae
ledesmx <alberto.ledesma.dev@gmail.com>
2026-08-09 17:56:04 -0600

    make the bullet start with scale 0 to 1 with a tween

--- metroid/scenes/bullet.gd
+++ metroid/scenes/bullet.gd
@@ -3,5 +3,9 @@ extends Node2D
 @export var direction := Vector2.RIGHT
 var speed = 100
 
+func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($".", "scale", Vector2.ONE, 0.1).from(Vector2.ZERO)
+
 func _process(delta: float) -> void:
 	position += direction * speed * delta
