74185420078e0a675dd638ea2a04fcb6d82b8fc0
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-10 20:27:31 -0600

    add drone tha follow the player

---
+++ metroid/scenes/entities/drone.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var attack_player := false
+var player_position := Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	if attack_player:
+		var total_velocity = sqrt((velocity.x ** 2) + (velocity.y ** 2))
+		velocity +=  (player_position - position).normalized() * 4
+		velocity = velocity.limit_length(100)
+		move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	attack_player = true
+	print("Player detected")
--- metroid/scenes/levels/level.gd
+++ metroid/scenes/levels/level.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
+@onready var player := $entities/Player
+@onready var drone := $Drone
 
+func _process(delta: float) -> void:
+	drone.player_position = player.position
 
 func _on_player_shoot(player_position: Vector2, direction: Vector2) -> void:
 	print(player_position)
