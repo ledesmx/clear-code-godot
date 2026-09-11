001d03a1a91b8997554487c2a6304733212a5f93
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-10 21:12:01 -0600

    add explotion animation for drone

--- metroid/scenes/entities/drone.gd
+++ metroid/scenes/entities/drone.gd
@@ -9,7 +9,14 @@ func _physics_process(delta: float) -> void:
 		velocity +=  (player_position - position).normalized() * 4
 		velocity = velocity.limit_length(100)
 		move_and_slide()
+	if Input.is_action_just_pressed("explote"):
+		play_explosion()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	attack_player = true
 	print("Player detected")
+	
+func play_explosion():
+	$BodySprite2D.visible = false
+	$ExplosionSprite2D.visible = true
+	$AnimationPlayer.play("explosion_animation")
