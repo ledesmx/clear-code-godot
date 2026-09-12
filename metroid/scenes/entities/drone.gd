e334166475d57c04a346aa0c4d399070bcc8af4e
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-12 09:04:48 -0600

    explote drone when it gets close to the player

--- metroid/scenes/entities/drone.gd
+++ metroid/scenes/entities/drone.gd
@@ -12,10 +12,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("explote"):
 		play_explosion()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	attack_player = true
-	print("Player detected")
-	
+
 func play_explosion():
 	$BodySprite2D.visible = false
 	$ExplosionSprite2D.visible = true
@@ -25,3 +22,12 @@ func play_explosion():
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "explosion_animation":
 		queue_free()
+
+
+func _on_detect_player_area_2d_body_entered(_body: Node2D) -> void:
+	attack_player = true
+	print("Player detected")
+
+
+func _on_attack_area_2d_body_entered(body: Node2D) -> void:
+	play_explosion()
