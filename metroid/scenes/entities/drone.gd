98e3cb80fc3a43066e0ae56c35fae410cc503101
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-12 11:30:40 -0600

    destructe drone after been hit by three bullets

--- metroid/scenes/entities/drone.gd
+++ metroid/scenes/entities/drone.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var attack_player := false
 var player_position := Vector2.ZERO
 var attack_direction := Vector2.ZERO
+var life := 3
 
 signal explote(direction: Vector2)
 
@@ -22,8 +23,7 @@ func play_explosion():
 	$CollisionShape2D.set_deferred("disabled", true)
 	$ExplosionSprite2D.visible = true
 	$AnimationPlayer.play("explosion_animation")
-	$AttackArea2D/CollisionShape2D.set_deferred("disabled", true)
-	explote.emit(attack_direction)
+	
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
@@ -38,3 +38,18 @@ func _on_detect_player_area_2d_body_entered(_body: Node2D) -> void:
 
 func _on_attack_area_2d_body_entered(body: Node2D) -> void:
 	play_explosion()
+	$AttackArea2D/CollisionShape2D.set_deferred("disabled", true)
+	explote.emit(attack_direction)
+
+
+func _on_detect_bullet_area_2d_area_entered(area: Area2D) -> void:
+	life -= 1
+	if life == 0:
+		play_explosion()
+	else:
+		$BodySprite2D.self_modulate = Color.CRIMSON
+		$ImpactFeedbackTimer.start()
+
+
+func _on_impact_feedback_timer_timeout() -> void:
+	$BodySprite2D.self_modulate = Color.WHITE
