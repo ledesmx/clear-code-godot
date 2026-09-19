dae07292ff3d82931920fa9dd758c91c9986d081
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-19 10:03:20 -0600

    add chain reaction to the drone explotion

--- metroid/scenes/entities/drone.gd
+++ metroid/scenes/entities/drone.gd
@@ -4,18 +4,16 @@ var attack_player := false
 var player_position := Vector2.ZERO
 var attack_direction := Vector2.ZERO
 var life := 3
+var nearby_drones = {}
 
 signal explote(direction: Vector2)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if attack_player:
-		var total_velocity = sqrt((velocity.x ** 2) + (velocity.y ** 2))
-		attack_direction =(player_position - position).normalized()
+		attack_direction = (player_position - position).normalized()
 		velocity +=  attack_direction * 4
 		velocity = velocity.limit_length(100)
 		move_and_slide()
-	if Input.is_action_just_pressed("explote"):
-		play_explosion()
 
 
 func play_explosion():
@@ -24,25 +22,37 @@ func play_explosion():
 	$ExplosionSprite2D.visible = true
 	$AnimationPlayer.play("explosion_animation")
 	
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "explosion_animation":
-		queue_free()
+	await $AnimationPlayer.animation_finished
+	queue_free()
+	
+func chain_reaction():
+	for drone in get_tree().get_nodes_in_group("Drones"):
+		if position.distance_to(drone.position) < 30:
+			drone.play_explosion()
+	
 
 
 func _on_detect_player_area_2d_body_entered(_body: Node2D) -> void:
 	attack_player = true
-	print("Player detected")
+	#print("Player detected")
 
 
 func _on_attack_area_2d_body_entered(body: Node2D) -> void:
-	play_explosion()
-	$AttackArea2D/CollisionShape2D.set_deferred("disabled", true)
-	explote.emit(attack_direction)
+	print(body.name)
+	if body.is_in_group("Drones"):
+		print(body.name)
+		nearby_drones[body.name] = body
+	else:
+		play_explosion()
+		$AttackArea2D/CollisionShape2D.set_deferred("disabled", true)
+		explote.emit(attack_direction)
 
+func _on_attack_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Drones"):
+		if nearby_drones.has(body.name):
+			nearby_drones.erase(body.name)
 
-func _on_detect_bullet_area_2d_area_entered(area: Area2D) -> void:
+func _on_detect_bullet_area_2d_area_entered(_area: Area2D) -> void:
 	life -= 1
 	if life == 0:
 		play_explosion()
