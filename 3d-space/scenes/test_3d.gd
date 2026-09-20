2f792865a02bd9dddccfa6739a9f9ad219c187ab
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-20 08:56:25 -0600

    complete exercise of donut

--- 3d-space/scenes/test_3d.gd
+++ 3d-space/scenes/test_3d.gd
@@ -1,4 +1,10 @@
 extends Node3D
 
+var color_status := 0.0
+
 func _process(delta: float) -> void:
 	$MeshInstance3D.rotate_y(1 * delta)
+	$DonutMeshInstance3D2.position.x += 1 * delta
+
+	$DonutMeshInstance3D2.mesh.material.albedo_color = Color(color_status, 0.0, 0.0, 1.0)
+	color_status += 0.005
