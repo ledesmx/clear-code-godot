11a4e6f5ed65dcb276157ceb64c399bd5adaaa18
ledesmx <alberto.ledesma.dev@gmail.com>
2026-09-19 21:34:37 -0600

    jump only once

--- metroid/scenes/entities/player.gd
+++ metroid/scenes/entities/player.gd
@@ -35,7 +35,7 @@ func _physics_process(delta: float) -> void:
 		if velocity.x == 0:
 			got_hit = false
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= jump_strength
 	
 	if not explosion_direction.is_zero_approx():
